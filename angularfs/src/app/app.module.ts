eaeae2d99447e9377bb3a72eb45fa76bd9fea1bc
Dilum De Silva <dilumdesilva.info@gmail.com>
2018-02-04 20:14:21 +0530

    made the connection with my firestore(firebase).

--- angularfs/src/app/app.module.ts
+++ angularfs/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { environment } from '../environments/environment';
+import { AngularFireModule } from 'angularfire2';
 
 import { AppComponent } from './app.component';
 
--- angularfs/src/environments/environment.ts
+++ angularfs/src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDLH50pM1ICOC63AzLqiZFYzlapi9qNwNA",
+    authDomain: "filestore-e30ce.firebaseapp.com",
+    databaseURL: "https://filestore-e30ce.firebaseio.com",
+    projectId: "filestore-e30ce",
+    storageBucket: "filestore-e30ce.appspot.com",
+    messagingSenderId: "700748808682"
+  }
 };
